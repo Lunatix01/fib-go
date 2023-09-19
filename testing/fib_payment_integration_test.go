67de69327adb38b1b1b1673a0096ede2c0f241a8
alandosman <aland.o@the-gw.com>
2023-09-19 22:04:52 +0300

    added tests

--- testing/fib_payment_integration_test.go
+++ testing/fib_payment_integration_test.go
@@ -89,3 +89,52 @@ func TestCreatePaymentFails(t *testing.T) {
 	assert.Equal(t, err.ErrorBody.Errors[0].Detail, "general_invalid_request_details")
 
 }
+
+func TestCheckPayment(t *testing.T) {
+	// given
+	client, _ := fib.New(clientID, clientSecret, true)
+	createdPaymentResponse, _ := client.CreatePayment(amount, currency, callbackURL)
+
+	// when
+	paymentCheckResponse, _ := client.CheckPayment(createdPaymentResponse.PaymentID)
+	paymentID := paymentCheckResponse.PaymentID
+	paymentStatus := paymentCheckResponse.Status
+	monetaryValue := paymentCheckResponse.MonetaryValue
+	currency := monetaryValue.Currency
+	amount := monetaryValue.Amount
+	declinedAt := paymentCheckResponse.DeclinedAt
+	paidAt := paymentCheckResponse.PaidAt
+	paymentDecliningReason := paymentCheckResponse.DecliningReason
+	paidBy := paymentCheckResponse.PaidBy
+	name := paidBy.Name
+	iban := paidBy.IBAN
+
+	// then
+	assert.Equal(t, reflect.TypeOf(paymentID), reflect.TypeOf(uuid.New()))
+	assert.Equal(t, paymentStatus, fib.UNPAID)
+	assert.NotNil(t, monetaryValue)
+	assert.Equal(t, currency, "IQD")
+	assert.Equal(t, amount, 1000)
+	assert.Empty(t, declinedAt)
+	assert.Empty(t, paidAt)
+	assert.Empty(t, paymentDecliningReason)
+	assert.Empty(t, name)
+	assert.Empty(t, iban)
+
+}
+
+func TestCheckPaymentNotFound(t *testing.T) {
+	// given
+	client, _ := fib.New(clientID, clientSecret, true)
+	fakeUUID := uuid.New()
+
+	// when
+	_, err := client.CheckPayment(fakeUUID)
+
+	// then
+	assert.NotEmpty(t, err.ErrorBody.TraceID)
+	assert.Equal(t, err.ErrorBody.Errors[0].Code, "PAYMENT_NOT_FOUND")
+	assert.Equal(t, err.ErrorBody.Errors[0].Title, "")
+	assert.Equal(t, err.ErrorBody.Errors[0].Detail, "")
+
+}
